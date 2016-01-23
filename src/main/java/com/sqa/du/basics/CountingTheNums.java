d81fa1c9d59fc3fdbad5177a32c4e1069c9f8e60
Dancalif <dancalif@gmail.com>
2016-01-23 15:29:56 -0800

    Counting nums app

---
+++ src/main/java/com/sqa/du/basics/CountingTheNums.java
@@ -0,0 +1,31 @@
+
+package com.sqa.du.basics;
+
+public class CountingTheNums {
+	public static void main(String[] args) {
+
+////		 Counting to 100 using FOR loop
+//		 for (int count = 0; count <= 100; count++) {
+//		 System.out.println(count);
+//		 }
+
+		
+////		 Counting to 100 using DO WHILE loop
+//		 int count = 0;
+//		 do {
+//		 System.out.println(count);
+//		 count++;
+//		 } while (count != 101);
+		
+		
+////		Counting to 100 using WHILE llop
+//		int count = 0;
+//		while (count < 101) {
+//			 System.out.println(count);
+//			 count++;
+//		}
+		
+
+
+	}
+}
